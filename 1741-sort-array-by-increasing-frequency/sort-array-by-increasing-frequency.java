2dd9bfbc2ce84f316aad57f2242b6a71f51584f7
subramanyaganesh <48938733+subramanyaganesh@users.noreply.github.com>
2024-07-27 10:15:43 -0500

    Time: 7 ms (67.48%) | Memory: 44.9 MB (6.63%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -2,32 +2,21 @@ class Solution {
     public int[] frequencySort(int[] nums) {
         Map<Integer,Integer> map=new HashMap<>();
         for(int i:nums)map.compute(i,(k,v)->v==null?1:v+1);
-
-        PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>((a,b)->a.getValue()-b.getValue());
-        pq.addAll(map.entrySet());
         List<Integer> answer=new ArrayList<>();
-        PriorityQueue<Map.Entry<Integer,Integer>> fq=new PriorityQueue<>((a,b)->b.getKey()-a.getKey());
+        PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>((a,b)->{
+            int val=Integer.compare(a.getValue(),b.getValue());
+            if(val==0){
+                return Integer.compare(b.getKey(),a.getKey());
+            }else return val;
+        });
+        pq.addAll(map.entrySet());
         while(!pq.isEmpty()){
             Map.Entry<Integer,Integer> entry=pq.poll();
-            if(!fq.isEmpty()&&entry.getValue()!=fq.peek().getValue()){
-                while(!fq.isEmpty()){
-                    Map.Entry<Integer,Integer> e=fq.poll();
-                    int m=e.getValue();
-                    while(m!=0){
-                        answer.add(e.getKey());
-                        m--;
-                    }
-                }
+            int m=entry.getValue();
+            while(m!=0){
+                answer.add(entry.getKey());
+                m--;
             }
-            fq.add(entry);
-        }
-        while(!fq.isEmpty()){
-                Map.Entry<Integer,Integer> e=fq.poll();
-                int m=e.getValue();
-                while(m!=0){
-                    answer.add(e.getKey());
-                    m--;
-                }
         }
         return answer.stream().mapToInt(a->a).toArray();
     }
