5e8ae6f3034c007d8be2caaf596a5329f8cdb67c
subramanyaganesh <48938733+subramanyaganesh@users.noreply.github.com>
2024-08-25 19:13:29 -0500

    Time: 5 ms (93.13%) | Memory: 44.5 MB (52.18%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
+
         Map<Integer,Integer> map=new HashMap<>();
-        for(int i:nums)map.compute(i,(k,v)->v==null?1:v+1);
-        int[] ans=new int[nums.length];
-        int k=0;
+
+        for(int i:nums){
+            map.compute(i,(k,v)->v==null?1:v+1);
+        }
         PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>((a,b)->{
-            int val=Integer.compare(a.getValue(),b.getValue());
-            if(val==0){
-                return Integer.compare(b.getKey(),a.getKey());
-            }else return val;
+            if(b.getValue()!=a.getValue()){
+                return a.getValue()-b.getValue();
+            }else{
+                return b.getKey()-a.getKey();
+            }
         });
+
         pq.addAll(map.entrySet());
+        int[] ans=new int[nums.length];
+        int j=0;
         while(!pq.isEmpty()){
             Map.Entry<Integer,Integer> entry=pq.poll();
-            int m=entry.getValue();
-            while(m!=0){
-                ans[k++]=entry.getKey();
-                m--;
+            for(int i=0;i<entry.getValue();i++){
+                ans[j++]=entry.getKey();
             }
         }
         return ans;
