acc6aba985005c3fe3c429add3ac9a995989a2f9
subramanyaganesh <48938733+subramanyaganesh@users.noreply.github.com>
2024-10-27 12:56:47 -0700

    Time: 2 ms (79.24%) | Memory: 44.6 MB (34.25%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,30 +1,26 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-
-        
-        List<List<List<Integer>>> cube=List.of(
-        List.of(new ArrayList<>(),new ArrayList<>(),new ArrayList<>()),
-        List.of(new ArrayList<>(),new ArrayList<>(),new ArrayList<>()),
-        List.of(new ArrayList<>(),new ArrayList<>(),new ArrayList<>()));
-
-
+        List<List<List<Character>>> cube=List.of(
+                                    List.of(new ArrayList<>(),new ArrayList<>(),new ArrayList<>()),
+                                    List.of(new ArrayList<>(),new ArrayList<>(),new ArrayList<>()),
+                                    List.of(new ArrayList<>(),new ArrayList<>(),new ArrayList<>()));
         for(int i=0;i<board.length;i++){
-            List<Integer> horizontal=new ArrayList<>();
-            List<Integer> vertical=new ArrayList<>();
-            for(int j=0;j<board.length;j++){
+            List<Character> h=new ArrayList<>();
+            List<Character> v=new ArrayList<>();
+            for(int j=0;j<board[0].length;j++){
                 if(board[i][j]!='.'){
-                    int mm=Integer.parseInt(board[i][j]+"");
-                    if(!horizontal.contains(mm))horizontal.add(mm);
-                    else return false;
-                    if(!cube.get(i/3).get(j/3).contains(mm))cube.get(i/3).get(j/3).add(mm);
-                    else return false;
+                    if(!h.contains(board[i][j])){
+                        h.add(board[i][j]);
+                    }else return false;
+                    if(!cube.get(i/3).get(j/3).contains(board[i][j])){
+                        cube.get(i/3).get(j/3).add(board[i][j]);
+                    }else return false;
                 }
                 if(board[j][i]!='.'){
-                    int nn=Integer.parseInt(board[j][i]+"");
-                    if(!vertical.contains(nn))vertical.add(nn);
-                    else return false;
-                }
-
+                    if(!v.contains(board[j][i])){
+                        v.add(board[j][i]);
+                    }else return false;
+                } 
             }
         }
         return true;
