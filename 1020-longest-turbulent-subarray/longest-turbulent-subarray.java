e81909ed6330862696ad7db7f74f7af39cba71cc
subramanyaganesh <48938733+subramanyaganesh@users.noreply.github.com>
2024-11-05 15:29:05 -0800

    Time: 23 ms (7.53%) | Memory: 48.4 MB (95.09%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
@@ -1,26 +1,44 @@
 class Solution {
     public int maxTurbulenceSize(int[] arr) {
-        if(arr.length<1)return 1;
-        int[] d=new int[arr.length-1];
-        int max=0;
 
-        //-5,-2,8,-3,1,0,-7,8
+        if(arr.length==1)return 1;
+        
+        int[] diff=new int[arr.length-1];
+
         for(int i=1;i<arr.length;i++){
-            d[i-1]=arr[i]-arr[i-1];
+            int d=arr[i]-arr[i-1];
+            if(d>0){
+                diff[i-1]=1;
+            }else if(d<0){
+                diff[i-1]=-1;
+            }else
+                diff[i-1]=0;
         }
-        Stack<Integer> stack=new Stack<>();
-
-        for(int i=0;i<d.length;i++){
-            if(stack.isEmpty()){
-               if(d[i]!=0) stack.add(d[i]);
+        
+        System.out.println(Arrays.toString(diff));
+        //100,100,100,100,101,100
+        ///0,0,0,1,-1
+        
+        
+        int max=0;
+        int count=0;
+        int start=diff[0];
+        for(int i=1;i<diff.length;i++){
+            if(start!=0){
+                if(start*-1==diff[i])
+                    count++;
+                else count=0;
             }
-            else if(stack.peek()>0&&d[i]<0||stack.peek()<0&&d[i]>0)stack.push(d[i]);
-            else{
-                max=Math.max(max,stack.size());
-                stack.clear();
-                if(d[i]!=0)stack.add(d[i]);
-            }
+            start=diff[i];
+            max=Math.max(max,count);
+            
+            
+            //System.out.println("max= "+max);
         }
-        return Math.max(max,stack.size())+1;        
+        if(max==0&&start==0){
+            return 1;
+        }else if(max==0&&start!=0){
+            return 2;
+        }else return max+2;
     }
 }
