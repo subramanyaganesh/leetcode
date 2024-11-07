61bd8c8fc2ab576b96ca666b3730ab411c14b31f
subramanyaganesh <48938733+subramanyaganesh@users.noreply.github.com>
2024-11-07 15:40:55 -0800

    Time: 1 ms (58.59%) | Memory: 43.5 MB (20.92%) - LeetSync

--- 2386-min-max-game/min-max-game.java
+++ 2386-min-max-game/min-max-game.java
@@ -1,11 +1,11 @@
 class Solution {
     public int minMaxGame(int[] nums) {
         while(nums.length>1){
-            List<Integer> list=new ArrayList<>();
+            int[] list=new int[nums.length/2];
             for(int j=0;j<nums.length/2;j++){
-                list.add(j%2==0?Math.min(nums[2*j],nums[2*j+1]):Math.max(nums[2*j],nums[2*j+1]));
+                list[j]=j%2==0?Math.min(nums[2*j],nums[2*j+1]):Math.max(nums[2*j],nums[2*j+1]);
             }
-            nums=list.stream().mapToInt(a->a).toArray();
+            nums=list;
         }
         return nums[0];
     }
