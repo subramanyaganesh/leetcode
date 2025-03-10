3eaec2c73dbc269a543ed04232a2b2876d6c676a
subramanyaganesh <48938733+subramanyaganesh@users.noreply.github.com>
2025-03-10 02:05:07 -0700

    Time: 7 ms (6.21%) | Memory: 42.3 MB (6.72%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -2,28 +2,20 @@ class Solution {
     public String decodeString(String s) {
         Stack<String> stack=new Stack<>();
         for(char c:s.toCharArray()){
-            if(c!=']'){
-                stack.add(c+"");
-            }
-            else if(c==']'){
+            if(Character.isDigit(c)){
+                if(!stack.isEmpty()&&stack.peek().matches("\\d+")){
+                    stack.push(stack.pop()+c);
+                }else stack.push(c+"");
+            }else if(c!=']'){
+                stack.push(c+"");
+            }else{
                 StringBuilder sb=new StringBuilder();
                 while(!stack.isEmpty()&&!stack.peek().equals("[")){
                     sb.insert(0,stack.pop());
                 }
-                //System.out.println("This is sb "+sb);
-                if(!stack.isEmpty()&&stack.peek().equals("["))stack.pop();
-                StringBuilder pb=new StringBuilder();
-                int num=-1;
-                try{
-                    while(!stack.isEmpty()){
-                        num=Integer.parseInt(stack.peek());
-                        stack.pop();
-                        pb.insert(0,num);
-                    }
-                }catch(Exception e){}
-                if(!pb.isEmpty())stack.push(String.valueOf(sb).repeat(Integer.parseInt(pb.toString())));
+                stack.pop();
+                stack.push(String.valueOf(sb.toString()).repeat(Integer.parseInt(stack.pop())));
             }
-            //System.out.println(stack);
         }
         StringBuilder ans=new StringBuilder();
         while(!stack.isEmpty()){
